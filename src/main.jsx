ad2e18f89f346a42332cc3c7290c3e43ae9c7951
Clent Sondia <clentsondia124@gmail.com>
2026-01-30 13:51:28 +0800

    Added new pages and a footer

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,18 @@ import { AuthProvider } from './AuthProvider.jsx'
 import ProtectedRoute from './ProtectedRoute.jsx'
 
 import App from './pages/App.jsx'
+import Home from './pages/Home.jsx'
+import Profile from './pages/Profile.jsx'
+import Bookmark from './pages/Bookmark.jsx'
+import Menu from './pages/Menu.jsx'
+
+import CreateAccount from './pages/CreateAccount.jsx'
+import FinishSetup from './pages/FinishSetup.jsx'
+import PageLoadguard from './ui/PageLoadGuard.jsx'
+
 import Landing from './pages/Landing.jsx'
 import Login from './pages/Login.jsx'
-import CreateAccount from './pages/CreateAccount.jsx'
 import Signup from './pages/Signup.jsx'
-import FinishSetup from './pages/FinishSetup.jsx'
-import Interface from './ui/Interface.jsx'
-import PageLoadguard from './ui/PageLoadGuard.jsx'
-import Submitted from './pages/Submitted.jsx'
 
 const router = createBrowserRouter([
   { path: '/', element: <Landing /> },
@@ -26,9 +30,18 @@ const router = createBrowserRouter([
   {
     element: <ProtectedRoute />,
     children: [
-      { path: '/app', element: <App /> },
       { path: '/create-account', element: <CreateAccount /> },
       { path: '/finish-setup', element: <FinishSetup /> },
+      { 
+        path: '/app', 
+        element: <App />,
+        children: [
+          { index: true, element: <Home />},
+          { path: 'profile', element: <Profile />},
+          { path: 'bookmarks', element: <Bookmark />},
+          { path: 'menu', element: <Menu />},
+        ]
+      },
     ]
   },
 ])
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react"
 import { supabase } from "../data/supabase-client"
 import { useAuth } from "../AuthProvider"
+import { BookmarkIcon, HomeIcon, Menu, User } from "lucide-react"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import clsx from "clsx"
+import { motion, AnimatePresence } from "motion/react"
 
 export default function App(){
     const { session, loading } = useAuth()  
+    const navigate = useNavigate()
+    const location = useLocation()
 
     if (loading) {
         return(
@@ -16,6 +22,25 @@ export default function App(){
     const [name, setName] = useState(null)
     const [gender, setGender] = useState(null)
     const [profile, setProfile] = useState(null)
+
+    const navItems = [
+        { name: "home", path: '/app', icon: HomeIcon, label: "Home" },
+        { name: "profile", path: '/app/profile', icon: User, label: "Profile" },
+        { name: "bookmark", path: '/app/bookmarks', icon: BookmarkIcon, label: "Bookmarks" },
+        { name: "menu", path: '/app/menu', icon: Menu, label: "Menu" },
+    ]
+
+    const currentView = navItems
+        .slice()
+        .sort((a ,b) => b.path.length - a.path.length)
+        .find((item) => location.pathname.startsWith(item.path))
+        ?.name ?? "home"
+
+    const handleNav = (item) => {
+        if (currentView !== item.name) {
+            navigate(item.path)
+        }
+    }
 
     const getInfo = async () => {
 
@@ -45,14 +70,43 @@ export default function App(){
 
     return(
         <>
-            <div className="block md:hidden flex flex-col justify-center items-center gap-2">
-                {profile && <img src={profile} className="size-45 object-cover overflow-hidden rounded-full border-2"/>}
-                <h1 className="text-white">Welcome, {name}. You are a {gender}.</h1>
-                <button onClick={handleOut}>Log out</button>
-            </div>
+            <div className="block md:hidden flex flex-col fixed inset-0 border-1">
+                <div className="flex-1 overflow-y">
+                    <Outlet />
+                </div>
 
-            <div className="hidden md:block">
-                <h1 className="text-white">Welcome, {name}</h1>
+                <div className="bg-neutral-950 shadow-lg flex items-center justify-between px-8 gap-2">
+                    {navItems.map((item) => (
+                       <div key={item.name} className="size-full flex flex-col items-stretch">
+                        <button
+                            onClick={() => handleNav(item)}
+                            aria-label={item.label}
+                            className={clsx(
+                            "relative flex-1 flex justify-center items-center border-0 rounded-[0] transition-colors",
+                            currentView === item.name
+                                ? "text-neutral-100"
+                                : "text-neutral-400 hover:bg-neutral-700 hover:text-neutral-100 cursor-pointer"
+                            )}
+                        >
+                            <item.icon className="w-6 h-6" />
+                        </button>
+
+                        {currentView === item.name && 
+                            <AnimatePresence>
+                                {currentView === item.name && (
+                                <motion.div
+                                    className="w-full h-1 bg-neutral-100"
+                                    initial={{ scaleX: 0, opacity: 0 }}
+                                    animate={{ scaleX: 1, opacity: 1 }}
+                                    exit={{ scaleX: 0, opacity: 0 }}
+                                    transition={{ duration: 0.2, ease: "easeOut" }}
+                                />
+                                )}
+                            </AnimatePresence>
+                        }
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     )
