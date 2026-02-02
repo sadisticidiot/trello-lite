65a0a7598d45e5b6a183764477cf653da5019665
Clent Sondia <clentsondia124@gmail.com>
2026-02-02 22:42:13 +0800

    New UI animation

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { supabase } from "../data/supabase-client"
 import { useAuth } from "../AuthProvider"
-import { BookmarkIcon, HomeIcon, Menu, Minus, User } from "lucide-react"
+import { HomeIcon, Menu, Minus, User } from "lucide-react"
 import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import clsx from "clsx"
 import { motion, AnimatePresence } from "motion/react"
@@ -19,6 +19,11 @@ export default function App(){
 
     const params = new URLSearchParams(location.search)
     const sheet = params.get("sheet")
+
+    const closeSheet = () => {
+        params.delete("sheet")
+        navigate(`?${params.toString()}`)
+    }
 
 
     const navItems = [
@@ -47,50 +52,64 @@ export default function App(){
                         <Outlet />
                     </div>
 
-                    {sheet === "new-post"
-                        ? (
-                            <div className="size-full">
-                                <div className="absolute inset-0 backdrop-blur-[2px] bg-black/60" />
-                            <div className="bg-neutral-900 w-full h-125 absolute bottom-0 rounded-t-[25px] flex flex-col items-center justify-center">
-                                <Minus className="size-10 scale-x-300 text-white/70" />
-                                <div className="flex-1" />
-                            </div>
-                            </div>
-                        ) : (
-                        <div className="absolute bottom-0 w-full bg-neutral-900 shadow-lg flex items-center justify-between px-3 gap-2">
-                        {navItems.map((item) => (
-                        <div key={item.name} className="size-full flex flex-col items-stretch">
-                            <button
-                                onClick={() => handleNav(item)}
-                                aria-label={item.label}
-                                className={clsx(
-                                "relative flex-1 flex justify-center items-center border-0 rounded-[0] transition-colors",
-                                currentView === item.name
-                                    ? "text-neutral-100"
-                                    : "text-neutral-400"
-                                )}
-                            >
-                                <item.icon className="w-6 h-10" />
-                            </button>
-
-                            {currentView === item.name && 
-                                <AnimatePresence>
-                                    {currentView === item.name && (
-                                    <motion.div
-                                        className="w-full h-1 bg-neutral-100"
-                                        initial={{ scaleX: 0, opacity: 0 }}
-                                        animate={{ scaleX: 1, opacity: 1 }}
-                                        exit={{ scaleX: 0, opacity: 0 }}
-                                        transition={{ duration: 0.2, ease: "easeOut" }}
+                    <AnimatePresence mode="wait">
+                        {sheet === "new-post" &&
+                                <div className="size-full">
+                                    <motion.div 
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        onClick={closeSheet} 
+                                        className="absolute inset-0 backdrop-blur-[2px] bg-black/60" 
                                     />
-                                    )}
-                                </AnimatePresence>
-                            }
-                            </div>
-                        ))}
-                        </div>
-                        )
-                    }
+
+                                    <motion.div 
+                                        initial={{ y: "100%" }}
+                                        animate={{ y: 0 }}
+                                        exit={{ y: "100%" }}
+                                        transition={{ duration: 0.2, ease: "easeInOut"}}
+                                        className="bg-neutral-900 w-full h-118 absolute bottom-0 rounded-t-[25px] flex flex-col items-center justify-center"
+                                    >
+                                        <Minus className="size-10 scale-x-300 text-white/70" />
+                                        <div className="flex-1" />
+                                    </motion.div>
+                                </div>
+                        }
+                    </AnimatePresence>
+
+                    {sheet !== "new-post" &&
+                        <div className="absolute bottom-0 w-full bg-neutral-900 shadow-lg flex items-center justify-between px-3 gap-2">
+                            {navItems.map((item) => (
+                                <div key={item.name} className="size-full flex flex-col items-stretch">
+                                    <button
+                                        onClick={() => handleNav(item)}
+                                        aria-label={item.label}
+                                        className={clsx(
+                                        "relative flex-1 flex justify-center items-center border-0 rounded-[0] transition-colors",
+                                        currentView === item.name
+                                            ? "text-neutral-100"
+                                            : "text-neutral-400"
+                                        )}
+                                    >
+                                        <item.icon className="w-6 h-10" />
+                                    </button>
+
+                                    {currentView === item.name && 
+                                        <AnimatePresence>
+                                            {currentView === item.name && (
+                                            <motion.div
+                                                className="w-full h-1 bg-neutral-100"
+                                                initial={{ scaleX: 0, opacity: 0 }}
+                                                animate={{ scaleX: 1, opacity: 1 }}
+                                                exit={{ scaleX: 0, opacity: 0 }}
+                                                transition={{ duration: 0.2, ease: "easeOut" }}
+                                            />
+                                            )}
+                                        </AnimatePresence>
+                                    }
+                                </div>
+                            ))}
+                        </div>}
                 </div>
             </div>
         </>
