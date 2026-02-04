6f27f9e3ecef752fb35be49e3f1183cf193d8896
Clent Sondia <clentsondia124@gmail.com>
2026-02-04 19:50:19 +0800

    Changed landing page bg and overall look

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -24,19 +24,20 @@ function DesktopDisplay() {
 function MobileDisplay() {
     return(
         <motion.div 
-            className="flex flex-col items-center justify-center fixed inset-0 bg-neutral-950 p-4"
+            className="flex flex-col items-center justify-center fixed inset-0 bg-[url(/landing-bg.png)] bg-cover bg-cover p-4"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}    
         >
-            <div className="flex gap-6 justify-between items-center fixed top-0 bg-pink-500 p-2 px-4 w-full z-10">
+            <div className="absolute inset-0 bg-gradient-to-b from-pink-500/60 from-20% via-black/90 via-50% to-black to-90%" />
+            <div className="flex gap-6 justify-between items-center fixed top-4 bg-neutral-950 p-4 px-6 w-9/10 rounded-full shadow-xl/30">
                 <img src={logo} className="size-10 rounded-full border-2 border-r-0 border-white/70" />
 
-                <button className="bg-teal-300 w-35 ring-1 ring-white text-black font-bold text-[13px]">Sign In</button>
+                <button className="w-35 bg-pink-600 font-bold text-[13px] rounded-full">Sign In</button>
             </div>
 
 
           <div className="flex justify-center items-center z-3">
-            <h1 className="text-[30px]">ewan ko na lang talaga</h1>
+            <h1 className="text-[30px]">Talaan</h1>
           </div>
 
             <div className="flex w-full justify-center items-center gap-2 z-3">
