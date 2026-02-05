5a622ed1b6022af4a0c210ffb7e10d12fd8c1903
Clent Sondia <clentsondia124@gmail.com>
2026-02-05 09:50:01 +0800

    Testing out animations

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,36 @@
+import { 
+    motion, useMotionValueEvent, 
+    useScroll, useTransform 
+} from "motion/react"
+import { useRef } from "react"
 
 export default function Landing() {
+    const scrollRef = useRef(null)
+
+    const { scrollY } = useScroll({
+        container: scrollRef
+    })
+
+    const scale = useTransform(scrollY, [150, 545, 650, 900], [0.2, 1, 1, 0.2])
+    const opacity = useTransform(scrollY, [150, 545], [0, 1])
+
     return(
-        <div className="h-[600vh] overscroll-contain overflow-y-auto no-scrollbar border-2">
-            Hi
-        </div>
+        <div 
+            ref={scrollRef}
+            className="h-screen overflow-y-auto 
+            overscroll-contain no-scrollbar p-4"
+        >
+            <div className="h-[120vh]" />
+
+            <motion.div 
+                className="flex items-center justify-center 
+                bg-neutral-900 p-4 rounded h-[20rem]"
+                style={{ scale, opacity }}
+            >
+                Hi
+            </motion.div>
+
+            <div className="h-[100vh]" />
+         </div>
     )
 }
