657f916382a3aa520492547307708432a5701a6b
Clent Sondia <clentsondia124@gmail.com>
2026-02-09 04:07:38 +0800

    Testing out new features

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -6,9 +6,9 @@ export default function Notes() {
         <div className="grid grid-cols-2
         gap-4 pb-30">
             {posts.map((p) => (
-                <div className="flex rounded-xl flex-col 
+                <div key={p.id} className="flex rounded-xl flex-col 
                 p-2 text-start border-2 border-white/40">
-                    <h1 className="p-0 text-start text-[20px] pb-4">{p.title}</h1>
+                    <h1 className="p-0 text-start text-[20px] pb-4">{ !p.title ? "Untitled" : p.title }</h1>
                     <span className="text-neutral-200/95">{p.post}</span>
                 </div>
             ))}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,20 @@
 import { useAuth } from "../AuthProvider"
 import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import clsx from "clsx"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { usePreviousPathname } from "../data/PrevRoute"
 import { motion, AnimatePresence } from "motion/react"
+import { Search } from "lucide-react"
 
 export default function Profile() {
     const prevPath = usePreviousPathname()
-    const { profile, name, profileLoading } = useAuth()
+    const { posts, profile, name, profileLoading } = useAuth()
     const navigate = useNavigate()
     const location = useLocation()
     const loc = location.pathname
+
+    const [searchInput, setSearchInput] = useState("")
+    const [isOpen, setIsOpen] = useState(false)
 
     const pages = [
         { name: "Notes", path: '/app/profile'},
@@ -31,11 +35,6 @@ export default function Profile() {
     const currentIndex = getIndex(location.pathname)
     const prevIndex = prevPath ? getIndex(prevPath) : currentIndex
 
-    const direction = 
-        currentIndex > prevIndex ? 1 :
-        currentIndex < prevIndex ? -1 :
-        0
-
     const currentView =
         resolveRoute(location.pathname)?.name ?? "Notes"
 
@@ -48,38 +47,52 @@ export default function Profile() {
     return(
         <div className="h-full flex flex-col no-scrollbar
         pb-30 justify-center p-2 overflow-y-auto">
-            <header className="flex justify-between 
-            relative p-2 px-20 items-end gap-5">
-                <div className="absolute left-2 top-1">
-                    <img src={profile} className="rounded-full
-                    size-12 border-2 border-white/20" />
+            {isOpen ? (
+                <div className="bg-neutral-900 p-2 flex justify-end">
+                    <button onClick={() => setIsOpen(false)}
+                    className="rounded-0 w-auto">
+                        ESC
+                    </button>
                 </div>
-                {pages.map((p) => (
-                    <div key={p.name} className="relative flex flex-col">
-                        <button onClick={() => handleNav(p)}
-                        className={clsx(
-                        "w-auto border-0 p-0",   
-                        currentView === p.name
-                        ? "text-[14px] text-white"
-                        : "text-[13px] text-white/40"
-                        )}>
-                            {p.name}
-                        </button>
+            ) : (
+                <header className="flex justify-between 
+                relative p-2 px-20 items-end gap-5">
+                    <div className="absolute left-2 top-1">
+                        <img src={profile} className="rounded-full
+                        size-12 border-2 border-white/20" />
 
-                        <AnimatePresence>
-                            {currentView === p.name && (
-                            <motion.div
-                                className="absolute top-6 w-full h-0.5 bg-neutral-100"
-                                initial={{ scaleX: 0, opacity: 0 }}
-                                animate={{ scaleX: 1, opacity: 1 }}
-                                exit={{ scaleX: 0, opacity: 0 }}
-                                transition={{ duration: 0.2, ease: "easeOut" }}
-                            />
-                            )}
-                        </AnimatePresence>
                     </div>
-                ))}
-            </header>
+                    {pages.map((p) => (
+                        <div key={p.name} className="relative flex flex-col">
+                            <button onClick={() => handleNav(p)}
+                            className={clsx(
+                            "w-auto border-0 p-0",   
+                            currentView === p.name
+                            ? "text-[14px] text-white"
+                            : "text-[13px] text-white/40"
+                            )}>
+                                {p.name}
+                            </button>
+
+                            <AnimatePresence>
+                                {currentView === p.name && (
+                                <motion.div
+                                    className="absolute top-6 w-full h-0.5 bg-neutral-100"
+                                    initial={{ scaleX: 0, opacity: 0 }}
+                                    animate={{ scaleX: 1, opacity: 1 }}
+                                    exit={{ scaleX: 0, opacity: 0 }}
+                                    transition={{ duration: 0.2, ease: "easeOut" }}
+                                />
+                                )}
+                            </AnimatePresence>
+                        </div>
+                    ))}
+                    <button className="absolute right-2 top-1
+                    w-auto border-0 p-0" onClick={() => setIsOpen(true)}>
+                        <Search />
+                    </button>
+                </header>
+            )}
 
             <div className="flex-1 h-full p-5">
                 <Outlet />
